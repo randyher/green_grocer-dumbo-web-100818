51963667a1ce36b2c95e5bf079e3c20021252412
Randy Herasme <randyherasme6@gmail.com>
2018-10-02 00:34:46 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,24 @@
 def consolidate_cart(cart)
-  # code here
+  output={}
+  count=Hash.new 0
+  price=Hash.new 0
+
+  cart.each do |items|
+    items.each do |food, info|
+      count[food]+=1
+      output[food]={
+      price: info[:price],
+      clearance: info[:clearance],
+      count: count[food]
+      }
+    end
+  end
+  
+
+  output
+
 end
+
 
 def apply_coupons(cart, coupons)
   # code here
